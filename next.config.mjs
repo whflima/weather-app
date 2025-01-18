2b8b64512052663f9f33283855f59c82b402d409
Welisson Hudson Fernandes Lima <wellissonsacer2000@gmail.com>
2025-01-17 22:46:04 -0300

    fix: added environment variables.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  },
 };
 /* module.exports = nextConfig*/
 export default nextConfig;
