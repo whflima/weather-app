91f251c9d98ecaa3afd355849140c39e175147b1
whflima <wellissonsacer2000@gmail.com>
2025-01-17 21:14:21 -0300

    Deploy git-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
-export const images = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // <=== enables static exports
+  images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'openweathermap.org',
-        },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
     ],
+  },
 };
+
+module.exports = nextConfig;
